d39a37fb94a4318b92d8fbc44eae0f9ee3d9523b
Alaa Ebrahim <alaaebrahim387@gmail.com>
2023-09-19 15:00:40 +0300

    Moved heroRepository from models directory to their own repository directory

---
+++ HeroGallery/Repositores/IHeroRepository.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using HeroManagement.Models;
+
+namespace HeroGallery.Repositores
+{
+    public interface IHeroRepository
+    {
+        Task<IEnumerable<Hero>> GetAllHeros();
+        Task<Hero> GetHero(int Id);
+		Task<Hero> AddHero(Hero Hero);
+        Hero UpdateHero(Hero HeroChanges);
+        Hero DeleteHero(int id);
+
+    }
+}   
---
+++ HeroGallery/Repositores/SqlHeroRepository.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using HeroManagement.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using Microsoft.Extensions.Logging;
+
+namespace HeroGallery.Repositores
+{
+    public class SqlHeroRepository : IHeroRepository
+    {
+        private readonly AppDbContext context;
+        private readonly ILogger<IHeroRepository> logger;
+
+        public SqlHeroRepository(AppDbContext context, ILogger<SqlHeroRepository> logger)
+        {
+            this.context = context;
+            this.logger = logger;
+        }
+        public async Task<Hero> AddHero(Hero Hero)
+        {
+            await context.Heros.AddAsync(Hero);
+            context.SaveChanges();
+            return Hero;
+        }
+
+        public Hero DeleteHero(int id)
+        {
+            Hero hero = context.Heros.Find(id);
+            if (hero != null)
+            {
+                context.Heros.Remove(hero);
+                context.SaveChanges();
+            }
+            return hero;
+
+        }
+
+        public async Task<IEnumerable<Hero>> GetAllHeros()
+        {
+            return await context.Heros.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<Hero> GetHero(int Id)
+        {
+            return await context.Heros.FindAsync(Id);
+        }
+
+        public Hero UpdateHero(Hero HeroChanges)
+        {
+            var hero = context.Heros.Update(HeroChanges);
+            hero.State = EntityState.Modified;
+            context.SaveChanges();
+            return HeroChanges;
+        }
+    }
+}
